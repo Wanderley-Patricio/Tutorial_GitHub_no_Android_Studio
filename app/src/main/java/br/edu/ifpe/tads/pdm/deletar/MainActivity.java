d7983099e1fde4a0875f70528f89a8825f45242b
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-04-17 12:12:30 -0300

    Primeiro commit - versao001.00 do programa.

---
+++ app/src/main/java/br/edu/ifpe/tads/pdm/deletar/MainActivity.java
@@ -0,0 +1,31 @@
+package br.edu.ifpe.tads.pdm.deletar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv_ola = findViewById(R.id.tv_ola);
+        Button btn_ok = findViewById(R.id.btn_ok);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_ola.setText("Olá mundo!!!");
+            }
+        });
+
+
+
+
+    }
+}
